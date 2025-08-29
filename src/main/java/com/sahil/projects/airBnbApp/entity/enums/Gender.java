dd57af015eb65a99b6652f895ba44b8850814880
sahil-patel620 <Sp453771@gmail.com>
2025-08-29 18:51:57 +0530

    Entities are created

---
+++ src/main/java/com/sahil/projects/airBnbApp/entity/enums/Gender.java
@@ -0,0 +1,7 @@
+package com.sahil.projects.airBnbApp.entity.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/com/sahil/projects/airBnbApp/entity/enums/Role.java
@@ -0,0 +1,7 @@
+package com.sahil.projects.airBnbApp.entity.enums;
+
+public enum Role {
+
+    GUEST,
+    HOTEL_MANAGER
+}
